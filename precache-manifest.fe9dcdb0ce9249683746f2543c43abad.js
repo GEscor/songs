54b6930543ae87f2570caf4a9cc081a1722e0368
GEscor <escor@inbox.ru>
2019-09-07 12:41:03 +0700

    Updates

---
+++ precache-manifest.fe9dcdb0ce9249683746f2543c43abad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e126f6d6e09f6d41f634eac7b76ecdd",
+    "url": "/songs/index.html"
+  },
+  {
+    "revision": "e546260ff7a35f90637a",
+    "url": "/songs/static/css/main.2a8fa256.chunk.css"
+  },
+  {
+    "revision": "ac2bd2627266f03f26a2",
+    "url": "/songs/static/js/2.55b1e8f2.chunk.js"
+  },
+  {
+    "revision": "e546260ff7a35f90637a",
+    "url": "/songs/static/js/main.f336ce96.chunk.js"
+  },
+  {
+    "revision": "0892fcefc4db2d8dd500",
+    "url": "/songs/static/js/runtime~main.43dd6e6f.js"
+  }
+]);
